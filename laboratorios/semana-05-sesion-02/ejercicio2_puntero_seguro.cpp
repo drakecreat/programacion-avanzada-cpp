7fdfd535d84f7a99a7390dfa46def3f403a031fe
drakecreat <drake.creat@gmail.com>
2026-09-16 12:54:05 +0000

    comit semana 5 sesion 2

--- laboratorios/semana-05-sesion-02/ejercicio2_puntero_seguro.cpp
+++ laboratorios/semana-05-sesion-02/ejercicio2_puntero_seguro.cpp
@@ -3,17 +3,19 @@
 // TODO: cambia esta funcion para que no devuelva la direccion de una
 // variable local. Usa memoria dinamica en su lugar, con new.
 int* obtenerLecturaSegura(int valorSensor) {
-    int lectura = valorSensor * 2;
-    return &lectura;
+    int*  lectura = new int ( valorSensor * 2);
+    return lectura;
 }
 
 int main() {
     int* resultado = obtenerLecturaSegura(10);
     std::cout << "Lectura: " << *resultado << std::endl;
+  
 
     // TODO: despues de terminar con "resultado", libera la memoria con
     // delete y asignale nullptr, para dejar constancia de que ya no
     // apunta a memoria valida.
-
+    delete resultado;
+    resultado = nullptr;
     return 0;
 }
